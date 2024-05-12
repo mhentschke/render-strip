54c8453cf53b9c2b72f115bae7ef0b10c0cd9d94
Matheus Hentschke <matheushentschke@gmail.com>
2024-05-12 10:42:28 -0300

    fixed release

--- make-dist.sh
+++ make-dist.sh
@@ -6,8 +6,9 @@ fi
 
 echo "preparing release $1"
 
-addon_zip="releases/Render Strip $1 - Do not unzip!.zip"
+addon_zip="Render Strip $1 - Do not unzip!.zip"
 mkdir releases/render-strip
 cp *.py releases/render-strip
-zip -r "$addon_zip" releases/render-strip -x '*/.git/*' -x '*/.DS_Store/*' -x '*/__pycache__/*' -x '*/releases/*'
-rm -r releases/render-strip
+cd releases
+zip -r "$addon_zip" render-strip -x '*/.git/*' -x '*/.DS_Store/*' -x '*/__pycache__/*' -x '*/releases/*'
+rm -r render-strip
